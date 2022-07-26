9b4de0954ea252d38f9435bb23968341ec15a8f6
lukeb <lbre0013@student.monash.edu>
2022-07-26 11:07:11 +1000

    Added level terrain object

---
+++ Racer/Assets/LevelTerrain.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTerrain : MonoBehaviour
+{
+    public float friction;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if collision is with vehicle component
+
+        // Change component rb drag
+    }
+}
