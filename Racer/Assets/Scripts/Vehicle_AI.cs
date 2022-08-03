d4a700d7ec6a8e46418cf849c6c0db3ed649cd44
Syzham <syzham3@gmail.com>
2022-08-04 06:32:06 +1000

    Updated Vehicle_AI to control Actuators in new vehicle

--- Racer/Assets/Scripts/Vehicle_AI.cs
+++ Racer/Assets/Scripts/Vehicle_AI.cs
@@ -3,30 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AITest : MonoBehaviour
+public class Vehicle_AI : MonoBehaviour
 {
-    [SerializeField] private Thruster thrust;
-
     public float vehicleTiltAngle;
     public float currentSpeed;
     public bool isGrounded;
 
     private Rigidbody2D _rb;
+    private VehicleCore _core;
+    private List<ActuatorModule> _actuator;
+    private bool b = true;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _core = GetComponent<VehicleCore>();
+    }
 
     private void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
+        _actuator = _core.Actuators;
     }
 
     private void FixedUpdate()
     {
         UpdateVariables();
-        
-        if (isGrounded)
-            thrust.EnableThrusters();
-        else
-            thrust.DisableThrusters();
-        
     }
 
     private void UpdateVariables()
