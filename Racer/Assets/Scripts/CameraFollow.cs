ad736dd9d076a3ff7464eca33bc313891a8ec7d2
Gunthorpe63210 <lukebjbrennan@gmail.com>
2022-08-06 22:16:09 +1000

    Added core build mode functions

--- Racer/Assets/Scripts/CameraFollow.cs
+++ Racer/Assets/Scripts/CameraFollow.cs
@@ -6,12 +6,6 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform Target;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
