c0423edad254c26245d7b415cbefb555f2fb231c
Gunthorpe63210 <lukebjbrennan@gmail.com>
2022-08-08 21:33:53 +1000

    Added finish flag

---
+++ Racer/Assets/Scripts/Level/SimulationController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationController : MonoBehaviour
+{
+    public Transform buildModeCamPos;
+    public VehicleCore playerVehicle;
+
+    public GameObject buildModeUI;
+    public GameObject raceUI;
+    public GameObject winUI;
+
+    public Transform raceProgressBar;
+    public Vector3 raceFinishPoint;
+
+    private bool inBuildMode = true;
+    private CameraFollow cameraFollow;
+    private float raceDistance;
+
+    private void Awake()
+    {
+        cameraFollow = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow>();
+    }
+    void Start()
+    {
+        EnterBuildMode();
+    }
+
+    private void Update()
+    {
+        raceProgressBar.transform.localScale = new Vector3(Mathf.Max((raceDistance - Vector3.Distance(playerVehicle.transform.position, raceFinishPoint)) / raceDistance,0), 1, 1);
+    }
+
+    /// <summary>
+    /// Locks the camera into the build mode area and activates the build mode UI
+    /// </summary>
+    public void EnterBuildMode()
+    {
+        inBuildMode = true;
+        cameraFollow.Target = buildModeCamPos;
+
+        buildModeUI.SetActive(true);
+        raceUI.SetActive(false);
+    }
+
+    /// <summary>
+    /// Hides the build mode UI and begins the simulation
+    /// </summary>
+    public void StartRace()
+    {
+        inBuildMode = false;
+        cameraFollow.Target = playerVehicle.transform;
+
+        buildModeUI.SetActive(false);
+        raceUI.SetActive(true);
+
+        raceDistance = Vector3.Distance(playerVehicle.transform.position, raceFinishPoint);
+        Debug.Log(raceDistance);
+    }
+
+    /// <summary>
+    /// Halts the simulation and displays the 'Win' UI
+    /// </summary>
+    public void WinRace()
+    {
+        raceUI.SetActive(false);
+        winUI.SetActive(true);
+    }
+}
