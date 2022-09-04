f56a853a8a3ebf93c4a435c26e0111f77cf6311e
Crowsinc <crowsinc.dev@gmail.com>
2022-09-05 00:18:43 +1000

    Cleaned up grid size loops

--- Racer/Assets/Scripts/Build Mode/VehicleConstructor.cs
+++ Racer/Assets/Scripts/Build Mode/VehicleConstructor.cs
@@ -26,13 +26,8 @@ public class VehicleConstructor : MonoBehaviour
         DraggableModule draggableModule = module.GetComponent<DraggableModule>();
 
         var gridPos = TransformToGrid(module.transform.position - draggableModule.CalculateRotationOffset());
-
-        // Transform the grid size to be lined up with the rotation of the module
-        var gridSize = new Vector2(
-            Mathf.Round(module.transform.TransformDirection(properties.Size).x),
-            Mathf.Round(module.transform.TransformDirection(properties.Size).y)
-         );
-
+        var gridSize = ReorientGridSize(properties.Size, module.transform.rotation);
+       
         return (gridSize, gridPos);
     }
 
@@ -45,11 +40,11 @@ public class VehicleConstructor : MonoBehaviour
     {
         var (gridSize, gridPos) = GatherGridSpaceInfo(module);
 
-        for (int dx = 0; dx < Mathf.Abs(gridSize.x); dx++)
+        for (int dx = 0; dx != gridSize.x; dx += Math.Sign(gridSize.x))
         {
-            for (int dy = 0; dy < Mathf.Abs(gridSize.y); dy++)
+            for (int dy = 0; dy != gridSize.y; dy += Math.Sign(gridSize.y))
             {
-                var coord = new Vector2Int(gridPos.x + Math.Sign(gridSize.x) * dx, gridPos.y + Math.Sign(gridSize.y) * dy);
+                var coord = new Vector2Int(gridPos.x + dx, gridPos.y + dy);
 
                 // If the coord is taken by a different object, or is outside the grid
                 if (!TestOnGrid(coord))
@@ -82,11 +77,11 @@ public class VehicleConstructor : MonoBehaviour
         );
 
         // Set size as unavailable module positions
-        for (int dx = 0; dx < Mathf.Abs(gridSize.x); dx++)
+        for (int dx = 0; dx != gridSize.x; dx += Math.Sign(gridSize.x))
         {
-            for (int dy = 0; dy < Mathf.Abs(gridSize.y) ; dy++)
+            for (int dy = 0; dy != gridSize.y; dy += Math.Sign(gridSize.y))
             {
-                var coord = new Vector2Int(gridPos.x + Math.Sign(gridSize.x) * dx, gridPos.y + Math.Sign(gridSize.y) * dy);
+                var coord = new Vector2Int(gridPos.x + dx, gridPos.y + dy);
                 _occupancy[coord] = module.GetInstanceID();
             }
         }
@@ -100,18 +95,13 @@ public class VehicleConstructor : MonoBehaviour
     /// <param name="size">size of the module</param>
     public void RemoveModule(Vector2Int gridPos, Vector2 size, float rotation)
     {
-        var rotSize = Quaternion.Euler(0, 0, rotation) * size;
-        var gridSize = new Vector2(
-           Mathf.Round(rotSize.x),
-           Mathf.Round(rotSize.y)
-        );
-
+        var gridSize = ReorientGridSize(size, Quaternion.Euler(0, 0, rotation));
         _design.Remove(gridPos);
-        for (int dx = 0; dx < Mathf.Abs(gridSize.x); dx++)
+        for (int dx = 0; dx != gridSize.x; dx += Math.Sign(gridSize.x))
         {
-            for (int dy = 0; dy < Mathf.Abs(gridSize.y); dy++)
+            for (int dy = 0; dy != gridSize.y; dy += Math.Sign(gridSize.y))
             {
-                var coord = new Vector2Int(gridPos.x + Math.Sign(gridSize.x) * dx, gridPos.y + Math.Sign(gridSize.y) * dy);
+                var coord = new Vector2Int(gridPos.x + dx, gridPos.y + dy);
                 _occupancy.Remove(coord);
             }
         }
@@ -130,6 +120,7 @@ public class VehicleConstructor : MonoBehaviour
 
     }
 
+
     /// <summary>
     /// Clamps the position of a module to a grid cell position
     /// </summary>
@@ -137,9 +128,12 @@ public class VehicleConstructor : MonoBehaviour
     /// <returns>the clamped position</returns>
     public static Vector2 ClampToGrid(Vector2 worldPos)
     {
-        return new Vector2(            Mathf.Round(worldPos.x),
+        return new Vector2(
+            Mathf.Round(worldPos.x),
             Mathf.Round(worldPos.y)
-        );    }
+        );
+    }
+
 
     /// <summary>
     /// Tests whether the given grid point is valid (on the grid space)
@@ -149,6 +143,25 @@ public class VehicleConstructor : MonoBehaviour
     public static bool TestOnGrid(Vector2 gridPos)
     {
         return gridPos.x > -7 && gridPos.x < 7 && gridPos.y > -4 && gridPos.y < 4;
+    }
+
+
+    /// <summary>
+    /// Re-orients the given module size based on the direction of the module rotation.
+    /// The width and height has directionality from the origin point of the module, 
+    /// meaning they can be negative depending on the rotation. 
+    /// </summary>
+    /// <param name="size"> the size of the module </param>
+    /// <param name="moduleRotation"> a Quaternion representing the rotation of the module </param>
+    /// <returns> The transformed grid size </returns>
+    public static Vector2 ReorientGridSize(Vector2 size, Quaternion moduleRotation)
+    {
+        var rotSize = moduleRotation * size;
+        var gridSize = new Vector2(
+           Mathf.Round(rotSize.x),
+           Mathf.Round(rotSize.y)
+        );
+        return gridSize;
     }
 
 
