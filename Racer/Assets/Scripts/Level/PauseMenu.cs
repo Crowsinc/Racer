55dd60a77304dff7d5e07ec15a6ce8c5e353bf0b
Gunthorpe63210 <lukebjbrennan@gmail.com>
2022-09-08 11:25:35 +1000

    Added functionality to pause menu

---
+++ Racer/Assets/Scripts/Level/PauseMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool _menuOpen = false;
+    private bool buttonDown = false;
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        Debug.Log(Input.GetAxisRaw("Cancel"));
+        if (Input.GetAxisRaw("Cancel") > 0 && !buttonDown)
+        {
+            buttonDown = true;
+            if (_menuOpen)
+            {
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+                _menuOpen = false;
+            }
+            else
+            {
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+                _menuOpen = true;
+            }
+        }
+        else if (!(Input.GetAxisRaw("Cancel") > 0))
+        {
+            buttonDown = false;
+        }
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadSceneAsync(GameConstants.MAIN_MENU_SCENE_ID);
+    }
+}
