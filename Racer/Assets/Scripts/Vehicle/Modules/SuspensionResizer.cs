9ccb4b36cb7e303a4f884b029bf6d4cbab5043d6
Crowsinc <crowsinc.dev@gmail.com>
2022-09-15 23:34:44 +1000

    Fixed suspension resizing

--- Racer/Assets/Scripts/Vehicle/Modules/SuspensionResizer.cs
+++ Racer/Assets/Scripts/Vehicle/Modules/SuspensionResizer.cs
@@ -14,16 +14,31 @@ public class SuspensionResizer : MonoBehaviour
     /// </summary>
     public GameObject SuspensionObject;
 
+    private Vector2 _origScale = Vector2.zero;
+
+
+    void Awake()
+    {
+        _origScale = SuspensionObject.transform.localScale;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (SliderJoint != null && SuspensionObject != null && SliderJoint.enabled)
+        if(SliderJoint == null || SuspensionObject == null)
+        {
+            Debug.LogError("Suspension object is misconfigured");
+            return;
+        }
+
+
+        if(SliderJoint.enabled)
         {
             SuspensionObject.transform.localScale = new Vector2(
                 SuspensionObject.transform.localScale.x,
                 SliderJoint.jointTranslation
             );
         }
-      //  else Debug.LogWarning("Suspension resizer is not configured");  
+        else SuspensionObject.transform.localScale = _origScale;
     }
 }
