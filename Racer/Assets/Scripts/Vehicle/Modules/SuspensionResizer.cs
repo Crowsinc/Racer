25d6c1469294381e13c4c86e89fade5be6cf8eb5
Crowsinc <crowsinc.dev@gmail.com>
2022-09-17 02:40:25 +1000

    Fixed suspension resizer

--- Racer/Assets/Scripts/Vehicle/Modules/SuspensionResizer.cs
+++ Racer/Assets/Scripts/Vehicle/Modules/SuspensionResizer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SuspensionResizer : MonoBehaviour
 {
@@ -15,28 +16,33 @@ public class SuspensionResizer : MonoBehaviour
     public GameObject SuspensionObject;
 
     private Vector2 _origScale = Vector2.zero;
-
+    private float _scaleRatio = 1.0f;
 
     void Awake()
     {
-        _origScale = SuspensionObject.transform.localScale;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(SliderJoint == null || SuspensionObject == null)
+        if (SliderJoint == null || SuspensionObject == null)
         {
             Debug.LogError("Suspension object is misconfigured");
             return;
         }
 
+        _origScale = SuspensionObject.transform.localScale;
+        _scaleRatio = (SliderJoint.limits.max - SliderJoint.limits.min) / _origScale.y;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
         if(SliderJoint.enabled)
         {
+            var p1 = SliderJoint.attachedRigidbody.transform.TransformPoint(SliderJoint.anchor);
+            var p2 = SliderJoint.connectedBody.transform.TransformPoint(SliderJoint.connectedAnchor);
+            var centre = 0.5f * (p1 + p2);
+            
+            SuspensionObject.transform.position = centre;
             SuspensionObject.transform.localScale = new Vector2(
                 SuspensionObject.transform.localScale.x,
-                SliderJoint.jointTranslation
+                SliderJoint.jointTranslation * _scaleRatio
             );
         }
         else SuspensionObject.transform.localScale = _origScale;
