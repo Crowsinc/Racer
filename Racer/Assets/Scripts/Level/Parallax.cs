efcaadb9af58d30ab53d1fc64999494f9687291d
Gunthorpe63210 <lukebjbrennan@gmail.com>
2022-09-18 21:37:44 +1000

    Added Parallax to Level namespace

--- Racer/Assets/Scripts/Level/Parallax.cs
+++ Racer/Assets/Scripts/Level/Parallax.cs
@@ -2,44 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
+namespace Level
 {
-    private float _length;
-    private float _startPos;
-    private Vector3 _initialPos;
-    private GameObject cam;
-    public float parallaxEffect;
-    private SimulationController _simController;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Parallax : MonoBehaviour
     {
-        cam = Camera.main.gameObject;
-        _startPos = transform.position.x;
-        _initialPos = transform.position;
-        _length = GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-        _simController = GameObject.FindGameObjectWithTag("GameController").GetComponent<SimulationController>();
-    }
+        private float _length;
+        private float _startPos;
+        private Vector3 _initialPos;
+        private GameObject cam;
+        public float parallaxEffect;
+        private SimulationController _simController;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_simController.inBuildMode)
+        // Start is called before the first frame update
+        void Start()
         {
-            transform.position = _initialPos;
-            return;
+            cam = Camera.main.gameObject;
+            _startPos = transform.position.x;
+            _initialPos = transform.position;
+            _length = GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+            _simController = GameObject.FindGameObjectWithTag("GameController").GetComponent<SimulationController>();
         }
-        float offset = cam.transform.position.x * (1 - parallaxEffect);
-        float dist = cam.transform.position.x * parallaxEffect;
-        transform.position = new Vector3(_startPos + dist, transform.position.y, transform.position.z);
 
-        if (offset > _startPos + _length)
+        // Update is called once per frame
+        void Update()
         {
-            _startPos += _length * 2;
-        }
-        else if (offset < _startPos - _length)
-        {
-            _startPos -= _length * 2;
+            if (_simController.inBuildMode)
+            {
+                transform.position = _initialPos;
+                return;
+            }
+            float offset = cam.transform.position.x * (1 - parallaxEffect);
+            float dist = cam.transform.position.x * parallaxEffect;
+            transform.position = new Vector3(_startPos + dist, transform.position.y, transform.position.z);
+
+            if (offset > _startPos + _length)
+            {
+                _startPos += _length * 2;
+            }
+            else if (offset < _startPos - _length)
+            {
+                _startPos -= _length * 2;
+            }
         }
     }
 }
